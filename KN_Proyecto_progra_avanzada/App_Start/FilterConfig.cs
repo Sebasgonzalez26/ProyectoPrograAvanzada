cac34271e220920177679cec6ec740f7e56e5b71
Sebasgonzalez26 <147010566+Sebasgonzalez26@users.noreply.github.com>
2025-09-29 11:41:21 -0600

    Pirmer commit, organizacion de archivos

---
+++ KN_Proyecto_progra_avanzada/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KN_Proyecto_progra_avanzada
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ KN_Proyecto_progra_avanzada/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KN_Proyecto_progra_avanzada.MvcApplication" Language="C#" %>
